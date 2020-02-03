a9d9268ae497ee083048505917cfa18c645c75e4
Kaushik Kulkarni <kgk2@illinois.edu>
2020-02-03 13:34:41 -0600

    use ints, simpler to visualize

--- tests/simple_struct.c
+++ tests/simple_struct.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 struct Coords {
-  float x, y, z;
+  int x, y, z;
 };
 
 int main()
 {
   struct Coords fav_3d_pt;
-  fav_3d_pt.x = 1.0/3.0;
-  fav_3d_pt.y = 2.0/3.0;
-  fav_3d_pt.z = 2.0/3.0;
+  fav_3d_pt.x = 14;
+  fav_3d_pt.y = 18;
+  fav_3d_pt.z = 22;
 
-  printf("My favorite coordinate are: (%f, %f, %f)\n", fav_3d_pt.x, fav_3d_pt.y, fav_3d_pt.z);
-  printf("Reason begin its 2-norm is: %f\n", fav_3d_pt.x*fav_3d_pt.x + fav_3d_pt.y*fav_3d_pt.y + fav_3d_pt.z*fav_3d_pt.z);
+  printf("My favorite coordinate are: (%d, %d, %d)\n", fav_3d_pt.x, fav_3d_pt.y, fav_3d_pt.z);
 
   return 0;
 }
